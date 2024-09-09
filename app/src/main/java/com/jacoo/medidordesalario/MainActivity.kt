40f652e904f82ec24e5a40dd315af7bd61c57885
jac0b12 <jacob289355@gmail.com>
2024-09-09 16:39:02 -0500

    Update

--- app/src/main/java/com/jacoo/medidordesalario/MainActivity.kt
+++ app/src/main/java/com/jacoo/medidordesalario/MainActivity.kt
@@ -3,9 +3,10 @@ package com.jacoo.medidordesalario
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
